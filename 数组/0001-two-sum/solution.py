7d69cea3b1c8913623c42890962f329b278b6f90
Wina <56862416+wuyn639@users.noreply.github.com>
2026-02-25 14:25:18 +0800

    Implement Two Sum solution with explanation
    Add solution for the 'Two Sum' problem with comments.

--- 数组/0001-two-sum/solution.py
+++ 数组/0001-two-sum/solution.py
@@ -1,1 +1,20 @@
+# 1. 两数之和
+# - 难度：简单
+# - 标签：数组、哈希表
+# - 题目链接：https://leetcode.cn/problem-list/array/
 
+# 思路
+# 1. 暴力：O(n^2)
+# 2. 哈希表：O(n)
+
+# 代码
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        d = {}
+        for i in range(n):
+            if target-nums[i] in d:
+                return [i, d[target-nums[i]]]
+            d[nums[i]] = i
+        return []
+      
