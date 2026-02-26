0886b26f0379564437b8970c2fa7154ed54c39e6
Wina <56862416+wuyn639@users.noreply.github.com>
2026-02-26 10:18:25 +0800

    Update 0016-threeSumClosest.py

--- 滑动窗口与双指针/0016-threeSumClosest.py
+++ 滑动窗口与双指针/0016-threeSumClosest.py
@@ -1,3 +1,14 @@
+# 16. 最接近的三数之和
+# - 难度：中等
+# - 专题：双指针
+# - 题目链接：https://leetcode.cn/problem-list/array/
+
+# 思路
+# 通过加和当前数组的minimum和maximum，每次循环可以看到O(n)的信息量，但是暴力穷举每次循环只能看到O(1)的信息量
+# 注意3个小优化可以大幅提升效率
+# 复杂度：时间O(n^2)，空间O(1)
+
+# 代码
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
